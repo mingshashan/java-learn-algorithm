875f45b2699c99596887ed8b9703a93b991d9b16
mingshashan <aikongfu@163.com>
2017-12-27 22:09:36 +0800

    add selection sort

--- src/com/ithink/learn/algorithm/sort/insert/StraightInsertionSort.java
+++ src/com/ithink/learn/algorithm/sort/insert/StraightInsertionSort.java
@@ -3,34 +3,26 @@ package com.ithink.learn.algorithm.sort.insert;
 import java.util.Arrays;
 
 public class StraightInsertionSort {
-	
+
 	public static int[] sort(int[] array) {
-		int index = 0;
 		int temp = 0;
-		for (int i = 0; i < array.length - 1; i++) {
-			index = i;
-			for (int j = i; j < array.length; j++) {
-				if (array[j] < array[index]) {
-					index = j;
+		for (int i = 1; i < array.length; i++) {
+			for (int j = i; j > 0; j--) {
+				if (array[j] < array[j - 1]) {
+					temp = array[j];
+					array[j] = array[j - 1];
+					array[j - 1] = temp;
 				}
 			}
-			
-			if (index == i) {
-				continue;
-			}
-			temp = array[i];
-			array[i] = array[index];
-			array[index] = temp;
 		}
-		
 		return array;
 	}
-	
+
 	public static void main(String[] args) {
-		int[] array = new int[]{7, 9, 1, 0, 7, 3, 6};
-		
+		int[] array = new int[] { 7, 9, 1, 0, 7, 3, 6 };
+
 		int[] sortedArr = sort(array);
-		
+
 		System.out.println(Arrays.toString(sortedArr));
 	}
 }
---
+++ src/com/ithink/learn/algorithm/sort/selection/SelectionSort.java
@@ -0,0 +1,35 @@
+package com.ithink.learn.algorithm.sort.selection;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+	public static int[] sort(int[] array) {
+		int index = 0;
+		int temp = 0;
+		for (int i = 0; i < array.length - 1; i++) {
+			index = i;
+			for (int j = i; j < array.length; j++) {
+				if (array[j] < array[index]) {
+					index = j;
+				}
+			}
+			
+			if (index == i) {
+				continue;
+			}
+			temp = array[i];
+			array[i] = array[index];
+			array[index] = temp;
+		}
+		
+		return array;
+	}
+	
+	public static void main(String[] args) {
+		int[] array = new int[]{7, 9, 1, 0, 7, 3, 6};
+		
+		int[] sortedArr = sort(array);
+		
+		System.out.println(Arrays.toString(sortedArr));
+	}
+}
